0af91f41209a09254cdd64e53d6836d65210efdd
OrlndoDacsta <orlandodacosta31@gmail.com>
2024-05-21 20:40:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">Hello World!</h1>
+    </div>
+  );
+};
+export default App;
