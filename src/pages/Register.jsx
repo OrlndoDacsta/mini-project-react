4cb5ddd61c50c32fd716317a953db68dcd2b9391
OrlndoDacsta <orlandodacosta31@gmail.com>
2024-05-23 20:43:40 +0700

    add notification and link  register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,12 +4,14 @@ import axios from "axios";
 import mail from "../assets/icons/mailLogin.svg";
 import lock from "../assets/icons/lockLogin.svg";
 import registerImg from "../assets/images/register.png";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
   const [error, setError] = useState("");
+  const [close, setClose] = useState(false);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -18,6 +20,10 @@ const Register = () => {
   const handlePasswordChange = (event) => {
     console.log(event);
     setPassword(event.target.value);
+  };
+
+  const handleClose = () => {
+    setClose(true);
   };
 
   const handleSubmit = () => {
@@ -41,13 +47,108 @@ const Register = () => {
   return (
     <div>
       <div className="content-center w-screen h-screen bg-emerald-900">
-        {token && <h1>Login Success</h1>}
-        {error && <h1>{error}</h1>}
         <div className="flex">
           <div className="flex flex-col items-center justify-center gap-5 w-110">
             <h1 className="mb-5 text-3xl font-bold text-white">
               Create Your Account
             </h1>
+
+            {token && (
+              <div className={close ? "hidden" : "flex"}>
+                <div
+                  id="toast-success"
+                  class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 gap-2"
+                  role="alert"
+                >
+                  <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+                    <svg
+                      class="w-5 h-5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span class="sr-only">Check icon</span>
+                  </div>
+                  <div class="ms-3 text-sm font-normal">Register Success</div>
+                  <button
+                    onClick={handleClose}
+                    type="button"
+                    class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-5 w-5 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                    data-dismiss-target="#toast-success"
+                    aria-label="Close"
+                  >
+                    <span class="sr-only">Close</span>
+                    <svg
+                      class="w-3 h-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 14"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            )}
+
+            {error && (
+              <div className={close ? "hidden" : "flex"}>
+                <div
+                  id="toast-danger"
+                  class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+                  role="alert"
+                >
+                  <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                    <svg
+                      class="w-5 h-5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+                    </svg>
+                    <span class="sr-only">Error icon</span>
+                  </div>
+                  <div class="ms-3 text-sm font-normal">{error}</div>
+                  <button
+                    onClick={handleClose}
+                    type="button"
+                    class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                    data-dismiss-target="#toast-danger"
+                    aria-label="Close"
+                  >
+                    <span class="sr-only">Close</span>
+                    <svg
+                      class="w-3 h-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 14"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            )}
+
             <div>
               <div className="mb-2">
                 <label htmlFor="email" className="text-white">
@@ -86,12 +187,15 @@ const Register = () => {
                 Create Account
               </button>
             </div>
+            <p className="text-white">
+              Already have an account?{" "}
+              <Link to="/login" className="text-yellow-500">
+                Login
+              </Link>
+            </p>
           </div>
-
           <img className="h-96" src={registerImg} alt="loginLogo" />
         </div>
-        {token && <p>Registered successfully!</p>}
-        {error && <p>{error}</p>}
       </div>
     </div>
   );
